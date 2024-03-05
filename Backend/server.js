d1d1f1d70257df7f304abdfc7bfa46a571a71158
enn-dee <nddar191131@gmail.com>
2024-03-05 14:52:35 +0530

    res path

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,7 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
